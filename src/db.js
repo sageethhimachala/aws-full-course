0d9c73c82bfce8f45ee89d24c4884bb311b6c6fd
Sageeth Himachala <sageethhimachala@gmail.com>
2026-07-30 21:18:28 +0530

    Change pool configuration to enable SSL

--- src/db.js
+++ src/db.js
@@ -8,10 +8,10 @@ const pool = new Pool({
     database: process.env.DB_NAME || 'url_shortener',
     password: process.env.DB_PASSWORD || 'password',
     port: process.env.DB_PORT || 5432,
-    ssl: false
-    // ssl: {
-    //     rejectUnauthorized: false // Allow self-signed AWS certificates
-    // }
+    // ssl: false
+    ssl: {
+        rejectUnauthorized: false // Allow self-signed AWS certificates
+    }
 })
 
 // Test the database connection
